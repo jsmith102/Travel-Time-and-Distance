63ffff32ec062a72947dee7b2de54dd257184fb7
jsmith102 <justinsmith102@gmail.com>
2020-06-09 16:57:44 +0100

    addition of locations mmvm

---
+++ Travel Time and Distance/Pages/Locations/AddLocations.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Travel_Time_and_Distance.Models;
+
+namespace Travel_Time_and_Distance.Pages.Locations
+{
+    public class AddLocationsModel : PageModel
+    {
+        [BindProperty]
+        public LocationsModel Locations { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Save Model to database
+
+
+            return RedirectToPage("/Index", new { StartLocation = Locations.LocationStart });
+        }
+    }
+}
